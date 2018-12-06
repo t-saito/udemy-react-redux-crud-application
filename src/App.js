5036b8c5e651694ac55cdd8a351084f7feb2f255
SAITO, Takashi <t-saito@esm.co.jp>
2018-12-06 18:07:57 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div>
-        Hello world
-      </div>
-    );
+      <React.Fragment>
+        <label>bbb</label>
+        <input type="text" onChange={() => {console.log('abbb')}}></input>
+      </React.Fragment>
+    )
   }
 }
 
